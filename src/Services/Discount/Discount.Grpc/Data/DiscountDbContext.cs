2a2796d5fef38704a2b2e06a6b97179f340414c3
Mohamad Nezhadhendi <m.nezhad@saadat.local>
2024-09-25 15:18:47 +0330

    added some methods into Discount service :)

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountDbContext(DbContextOptions<DiscountDbContext> options) : DbContext(options)
+    {
+        public DbSet<Coupon> Coupons { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(new List<Coupon>
+            {
+                new()
+                {
+                    Id = 1,
+                    ProductName = "Iphone X",
+                    Description = "Iphone Discount",
+                    Amount = 150
+                },
+                new()
+                {
+                    Id = 2,
+                    ProductName = "Samsung 10",
+                    Description = "Samsung Discount",
+                    Amount = 100
+                }
+            });
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,38 @@
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService(DiscountDbContext dbContext, ILogger<DiscountService> logger): DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await dbContext.Coupons.FirstOrDefaultAsync(s => s.ProductName == request.ProductName) ?? new Coupon()
+            {
+                ProductName = "Not Found",
+                Amount = 0,
+                Description = "NotFound Product"
+            };
+            var response = coupon.Adapt<CouponModel>();
+            return response;
+        }
+
+        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            return base.CreateDiscount(request, context);
+        }
+
+        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            return base.UpdateDiscount(request, context);
+        }
+
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }
+    }
+}
