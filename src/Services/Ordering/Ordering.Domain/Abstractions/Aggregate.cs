91e40a8d5a92a44d9d14281826b8bbc4886ffdbe
Mohamad Nezhadhendi <m.nezhad@saadat.local>
2024-09-30 10:11:32 +0330

    Developed Ordering.Domain base on DDD pattern and its rules!

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TId>: Entity<TId>, IAggregate<TId>
+    {
+        private readonly List<IDomainEvent> _domainEvents = [];
+        public IReadOnlyList<IDomainEvent> DomainEvents => this._domainEvents.AsReadOnly();
+
+        public void AddDomainEvent(IDomainEvent domainEvent) => this._domainEvents.Add(domainEvent);
+
+        public IDomainEvent[] ClearDomainEvents()
+        {
+            var dequeuedEvents = this._domainEvents.ToArray();
+            this._domainEvents.Clear();
+            return dequeuedEvents;
+        }
+    }
+}
