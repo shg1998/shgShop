628bc2e545b4daea492c54664b12db2e91e08868
Mohamad Nezhadhendi <m.nezhad@saadat.local>
2024-10-07 14:35:19 +0330

    develop saveChanges Interceptor in Infrastructure Order project!

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Ordering.Domain.Abstractions;
+
+namespace Ordering.Infrastructure.Data.Interceptors
+{
+    public class AuditableEntityInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            UpdateEntities(eventData.Context);
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            UpdateEntities(eventData.Context);
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        private void UpdateEntities(DbContext? eventDataContext)
+        {
+            if(eventDataContext == null) return;
+            foreach (var entry in eventDataContext.ChangeTracker.Entries<IEntity>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedBy = "shg1998";
+                    entry.Entity.CreatedAt = DateTime.UtcNow;
+                }
+
+                if (entry.State != EntityState.Added && entry.State != EntityState.Modified &&
+                    !entry.HasChangedOwnedEntities()) continue;
+                entry.Entity.CreatedBy = "shg1998";
+                entry.Entity.CreatedAt = DateTime.UtcNow;
+            }
+        }
+    }
+
+    public static class Extensions
+    {
+        public static bool HasChangedOwnedEntities(this EntityEntry entry) =>
+            entry.References.Any(s =>
+                s.TargetEntry != null && s.TargetEntry.Metadata.IsOwned() &&
+                s.TargetEntry.State is EntityState.Added or EntityState.Modified);
+    }
+}
--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Data.Interceptors;
 
 namespace Ordering.Infrastructure
 {
@@ -12,6 +13,7 @@ namespace Ordering.Infrastructure
             var connectionString = configuration.GetConnectionString("Database");
             services.AddDbContext<ApplicationDbContext>(options =>
             {
+                options.AddInterceptors(new AuditableEntityInterceptor());
                 options.UseSqlServer(connectionString);
             });
             return services;
